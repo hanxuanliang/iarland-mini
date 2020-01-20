96c85f012aa7c08d92692b2d9e47d021d6765b89
hanhotfox <hxuanliang@163.com>
2020-01-20 16:09:20 +0800

    :lipstick: 增加like组件

---
+++ components/like/index.js
@@ -0,0 +1,23 @@
+// components/like/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    yesSrc: 'image/like.png',
+    noSrc: 'image/like@dis.png'
+  },
+
+  methods: {
+    onLike: function(event) {
+      
+    }
+  }
+})
